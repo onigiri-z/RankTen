0d9206fc7cbef95494e8b15cacaacce82d003ebb
onigiri-z <k.tennis218@icloud.com>
2021-11-19 16:34:02 +0900

    Initial Commit

---
+++ RankTen/RankTenApp.swift
@@ -0,0 +1,17 @@
+//
+//  RankTenApp.swift
+//  RankTen
+//
+//  Created by 入江健太 on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct RankTenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
